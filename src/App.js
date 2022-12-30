ce596241160e92414019f74721ee371ed36ba9b2
Alejo Viola <85246896+alejoviola@users.noreply.github.com>
2022-12-30 18:50:35 -0300

    Complete the Challenge

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useEffect, useState} from 'react';
 import './App.scss';
 import Species from './Species';
 
@@ -15,11 +16,101 @@ const SPECIES_IMAGES = {
 };
 const CM_TO_IN_CONVERSION_RATIO = 2.54;
 
+//////////////////////////////////////
+
+// CUSTOM HOOK //
+const useSpeciesList = url => {
+  const [speciesList, setSpeciesList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const solveData = async () => {
+    setIsLoading(true);
+    console.log('Fetching movie data...');
+    const speciesAPIArray = await getSpeciesAPI();
+    console.log('Fetching species...');
+    const promises = await speciesAPIArray.map(value => {
+      return new Promise(async (resolve, reject) => {
+        const response = await fetch(value, {
+          method: 'GET',
+        });
+        const species = await response.json();
+        resolve(species);
+      });
+    });
+    Promise.all(promises).then(value => {
+      setSpeciesList(value);
+      setIsLoading(false);
+      console.log('Done');
+    });
+  };
+
+  const getSpeciesAPI = async () => {
+    const response = await fetch(url, {
+      method: 'GET',
+    });
+    const {species} = await response.json();
+
+    const speciesAPIArray = [];
+
+    species.map(value => {
+      speciesAPIArray.push(value);
+    });
+    return speciesAPIArray;
+  };
+
+  useEffect(() => {
+    solveData();
+  }, []);
+
+  return {
+    speciesList,
+    isLoading,
+  };
+};
+
+//////////////////////////////////////
+
+// APP //
 function App() {
+  const {speciesList, isLoading} = useSpeciesList(API_URL);
+
   return (
     <div className="App">
       <h1>Empire Strikes Back - Species Listing</h1>
-      <div className="App-species" />
+      <div className="App-species">
+        {!isLoading ? (
+          speciesList.map(value => {
+            return (
+              <Species
+                key={value.name}
+                name={value.name}
+                classification={value.classification}
+                designation={value.designation}
+                height={
+                  !isNaN(value.average_height)
+                    ? `${(
+                        value.average_height / CM_TO_IN_CONVERSION_RATIO
+                      ).toFixed(0)}"`
+                    : 'N/A'
+                }
+                image={
+                  value.name.indexOf(`'`) > 0
+                    ? SPECIES_IMAGES[
+                        value.name
+                          .slice(0, value.name.indexOf(`'`))
+                          .toLowerCase()
+                      ]
+                    : SPECIES_IMAGES[value.name.toLowerCase()]
+                }
+                numFilms={value.films.length}
+                language={value.language}
+              />
+            );
+          })
+        ) : (
+          <div className="Loader" />
+        )}
+      </div>
     </div>
   );
 }
